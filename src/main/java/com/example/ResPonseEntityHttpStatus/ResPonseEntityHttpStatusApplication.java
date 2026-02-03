4678950a4c7ea3a0d8889ed337b475185e5d48b9
Prajjwal Sharma <prajjwalsharm.code01@gmail.com>
2026-02-03 11:54:19 +0530

    first commit

---
+++ src/main/java/com/example/ResPonseEntityHttpStatus/ResPonseEntityHttpStatusApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResPonseEntityHttpStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResPonseEntityHttpStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResPonseEntityHttpStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ResPonseEntityHttpStatus/ResPonseEntityHttpStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ResPonseEntityHttpStatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResPonseEntityHttpStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
